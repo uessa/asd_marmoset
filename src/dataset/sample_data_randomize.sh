614a227f9239be5e45792ab1e2e7c719841c7f89
Hideto-kawauchi <hidekureiskmt068@gmail.com>
2021-07-06 13:32:09 +0900

    発声の種類の分類まで一通り

--- src/dataset/sample_data_randomize.sh
+++ src/dataset/sample_data_randomize.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python data_randomize.py \
-       "/datanet/users/hkawauchi/vad_marmoset/raw/ayapyon_calltype_wav" \
-       "/datanet/users/hkawauchi/vad_marmoset/datasets/subset_ayapyon_calltype/raw" \
+       "/datanet/users/hkawauchi/vad_marmoset/raw/ayapyon_phee_wav" \
+       "/datanet/users/hkawauchi/vad_marmoset/datasets/subset_ayapyon_phee/raw" \
        0.7 0.1 0.2 \
        -l 1
--- src/dataset/sample_makedata.sh
+++ src/dataset/sample_makedata.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python makedata.py "subset_ayapyon_calltype" \
+python makedata.py "subset_ayapyon_phee" \
        -r 96000 \
        -l 2048 \
        -s 1024 \
