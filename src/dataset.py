5d5f872cf91cddaa7569ad630d48a789f94750ef
Hideto-kawauchi <hidekureiskmt068@gmail.com>
2021-07-08 12:04:10 +0900

    窓長1024に変更して発声の種類の分類

--- src/dataset.py
+++ src/dataset.py
@@ -41,7 +41,7 @@ class Mydatasets(torch.utils.data.Dataset):
         return outdata_spec, outdata_label
 
 if __name__ == "__main__":
-    p = path.path('subset_ayapyon_phee_fftlen1024')
+    p = path.path('subset_ayapyon_calltype_fftlen1024')
     train = p.train
     mydataset = Mydatasets(train, 'cnn')
     trainloader = torch.utils.data.DataLoader(
