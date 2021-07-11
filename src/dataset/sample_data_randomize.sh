f8f0d5e12f8175c5630dd77b4d9e3ef2fd0a943a
Hideto-kawauchi <hidekureiskmt068@gmail.com>
2021-07-11 15:03:25 +0900

    窓長16384に変更してPheeの分類

--- src/dataset/sample_data_randomize.sh
+++ src/dataset/sample_data_randomize.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python data_randomize.py \
-       "/datanet/users/hkawauchi/vad_marmoset/raw/ayapyon_vad_fftlen16384_wav" \
-       "/datanet/users/hkawauchi/vad_marmoset/datasets/subset_ayapyon_vad_fftlen16384/raw" \
+       "/datanet/users/hkawauchi/vad_marmoset/raw/ayapyon_phee_fftlen16384_wav" \
+       "/datanet/users/hkawauchi/vad_marmoset/datasets/subset_ayapyon_phee_fftlen16384/raw" \
        0.7 0.1 0.2 \
        -l 1
