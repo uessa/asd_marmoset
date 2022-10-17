194d6958f2d87200da241a342b8f7f8f5cb5f86b
uessa <minato.uesaka@gmail.com>
2022-10-17 22:36:53 +0900

    phee-trill, trill-phee, unknown除外のフレーム可視化（非対称）

--- src/others/show_frame_singleweek.py
+++ src/others/show_frame_singleweek.py
@@ -24,12 +24,23 @@ if __name__ == "__main__":
                "松島": "matsushima",}
     vpakids_eng = {"つぐみ": "tsugumi", "ひばり": "hibari",}
     marmo_eng = {**ue_eng, **sal_eng, **vpa_eng, **vpakids_eng}
-    call_label = {0: "No Call", 1: "Phee", 2: "Trill", 3: "Twitter", 4: "Other Calls"}
+    call_label = {0: "No Call", 1: "Phee", 2: "Trill", 3: "Twitter", 4: "Other Calls", 5:"Phee-Trill", 6:"Trill-Phee", 7:"Unknown"}
+    trains = ["カルビ","あいぴょん","真央","ブラウニー","花月","黄金","阿伏兎", 
+                "テレスドン","スカイドン","三春","会津","マティアス","エバート","ぶた玉","信成"]
+    valids = ["鶴ヶ城","ミコノス","イカ玉"]
+    tests = ["あやぴょん","ビスコッテイー","ドラコ","マルチナ","梨花"]
+    lnames = ["Phee","Trill","Twitter","Other Calls"]
+    vpas = ["高萩","平磯","阿字ヶ浦","馬堀","三崎","ひばり","つぐみ","日向夏","八朔","桂島","松島"]
+
 
     is_plot = 1 # plot on/off
-    labelpath = "/home/muesaka/projects/marmoset/datasets/subset_marmoset_11vpa_check_othercalls/test/" # GroundTruth frame .txt Path
-    resultpath = "/home/muesaka/projects/marmoset/datasets/subset_marmoset_11vpa_check_othercalls/test/results/" # Estimate frame .txt Path
-    pdf = PdfPages(labelpath + '23ue_test_5ue.pdf') # filename
+
+    labelpath = "/home/muesaka/projects/marmoset/datasets/subset_marmoset_11vpa/test_wo_pheetrill/" # GroundTruth frame .txt Path
+    resultpath = "/home/muesaka/projects/marmoset/datasets/subset_marmoset_11vpa/test/results/" # Estimate frame .txt Path
+    # labelpath = "/home/muesaka/projects/marmoset/datasets/subset_marmoset_23ue_hkawauchi/test_wo_pheetrill/labels/" # GroundTruth frame .txt Path
+    # resultpath = "/home/muesaka/projects/marmoset/datasets/subset_marmoset_23ue_hkawauchi/test_wo_pheetrill/results/" # Estimate frame .txt Path
+
+    # pdf = PdfPages(labelpath + 'frame_5ue.pdf') # filename
 
     files = [f for f in os.listdir(labelpath) if os.path.isfile(os.path.join(labelpath, f)) and f[-3:] == "txt"] # 末尾3文字まで（.txt）マッチ
     results = [f for f in os.listdir(resultpath) if os.path.isfile(os.path.join(resultpath, f)) and f[-3:] == "txt"] # 末尾3文字まで（.txt）マッチ
@@ -54,9 +65,6 @@ if __name__ == "__main__":
             date[0],
         )
 
-        # if date[0] != "ビスコッテイー_3W":
-        #     continue
-
         if is_plot: #is_plot = 1でプロット
 
             # sec translate
@@ -73,41 +81,34 @@ if __name__ == "__main__":
             plt.figure(figsize=(50,3),dpi=400) # plot figure size
             title = "{} (weeks={}, acc={}%)".format(marmo_eng[name[0]], date[0], acc) # plot title
 
-            # plot1
+            # Ground Truth Label
             plt.subplot(2, 1, 1)
-            plt.plot(label_sec, label == 1, "b",label="1:"+call_label[1],lw=lw)
-            plt.plot(label_sec, label == 2, "g",label="2:"+call_label[2],lw=lw)
-            plt.plot(label_sec, label == 3, "r",label="3:"+call_label[3],lw=lw)
-            plt.plot(label_sec, label == 4, "c",label="4:"+call_label[4],lw=lw)
+            plt.plot(label_sec, label == 1, "b", label="1:"+call_label[1], lw=lw) # Phee
+            plt.plot(label_sec, label == 2, "g", label="2:"+call_label[2], lw=lw) # Trill
+            plt.plot(label_sec, label == 3, "r", label="3:"+call_label[3], lw=lw) # Twitter
+            plt.plot(label_sec, label == 5, "c", label="5:"+call_label[5], lw=lw) # Phee-Trill
+            plt.plot(label_sec, label == 6, "m", label="6:"+call_label[6], lw=lw) # Trill-Phee
+            # plt.plot(label_sec, label == 4, "c", label="4:"+call_label[4], lw=lw) # Other Calls
             plt.xlim([x_min,x_max])
             plt.yticks([0,1],["NoCall","Call"])
             plt.title(title)
+            plt.legend(fontsize=16, bbox_to_anchor=(0, -.2), loc='upper left', ncol=5, )
             plt.ylabel("G.T.")
 
-            # plot2
+            # Estimated Label
             plt.subplot(2, 1, 2)
-            plt.plot(results_sec, results == 1, "b",label="1:"+call_label[1],lw=lw)
-            plt.plot(results_sec, results == 2, "g",label="2:"+call_label[2],lw=lw)
-            plt.plot(results_sec, results == 3, "r",label="3:"+call_label[3],lw=lw)
-            plt.plot(results_sec, results == 4, "c",label="4:"+call_label[4],lw=lw)
+            plt.plot(results_sec, results == 1, "b",label="1:"+call_label[1], lw=lw) # Phee
+            plt.plot(results_sec, results == 2, "g",label="2:"+call_label[2], lw=lw) # Trill
+            plt.plot(results_sec, results == 3, "r",label="3:"+call_label[3], lw=lw) # Twitter
+            # plt.plot(results_sec, results == 4, "c",label="4:"+call_label[4], lw=lw) # Other Calls
             plt.xlim([x_min,x_max])
             plt.yticks([0,1],["NoCall","Call"])
             plt.ylabel("Est.")
-            plt.legend(bbox_to_anchor=(1, -.2), loc='upper right', ncol=4)
             plt.xlabel("Time (sec) ")
 
             # plot option after plot1,2
             plt.tight_layout() # plot layout spacing
-            pdf.savefig() # plot to PDF saving
-            # dirpath = labelpath + "show_frames/"
-            # os.makedirs(dirpath, exist_ok=True)
-
-            # filename1 = dirpath + "show_frame_" + date[0] + "_sec_" + str(x_min) + "-" + str(int(x_max))
-            # # filename1 = dirpath + "show_frame_" + date[0]
-            # filename2 = filename1 + "_" + str(acc) + "%" + ".png"
-            # plt.savefig(filename2)
+            plt.savefig(labelpath + "frame_" + marmo_eng[name[0]] + date[0] + ".pdf")
             plt.close()
             
-
-    pdf.close()
 
