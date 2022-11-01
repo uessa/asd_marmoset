3b1569370d406f7a862e94c87789bfce34a54154
uessa <minato.uesaka@gmail.com>
2022-11-01 17:51:36 +0900

    最新の確認

--- src/test_for_one_data.sh
+++ src/test_for_one_data.sh
@@ -3,4 +3,4 @@
 python test_for_one_data.py \
        subset_marmoset_11vpa test \
        --model ../models/subset_marmoset_23ue/trial04/model.pth \
-       --batch_size 1 \
+       --batch_size 1
--- src/train.sh
+++ src/train.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 nohup python train.py \
-       subset_marmoset_23ue \
+       subset_marmoset_23ue_muesaka \
        --batch_size 2 \
        --lr 0.01 \
        > out.log \
        2> err.log \
        < /dev/null \
-       & \
+       &
