097804629ce325ce3a4070de43482ee5399d2d2c
uessa <minato.uesaka@gmail.com>
2022-11-01 17:51:05 +0900

    最新の確認

--- src/dataset/sample_data_randomize.sh
+++ src/dataset/sample_data_randomize.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python data_randomize.py \
-       "/datanet/users/hkawauchi/vad_marmoset/raw/test" \
-       "/datanet/users/hkawauchi/vad_marmoset/datasets/subset_test/raw" \
+       "/home/muesaka/projects/hkawauchi/marmoset/raw/marmoset_11vpa_wav" \
+       "/home/muesaka/projects/hkawauchi/marmoset/datasets/subset_marmoset_11vpa/raw" \
        0.7 0.1 0.2 \
        -l 1
--- src/dataset/sample_makedata.sh
+++ src/dataset/sample_makedata.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python makedata.py "subset_marmoset_5VPA" \
+python makedata.py "subset_marmoset_23ue_muesaka" \
        -r 96000 \
        -l 2048 \
        -s 1024 \
