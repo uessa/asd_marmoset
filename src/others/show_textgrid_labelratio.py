f29ce593df7a656867a12cdd46cc34d930cc6272
uessa <minato.uesaka@gmail.com>
2022-11-20 22:16:26 +0900

    個体ごとの積み上げ棒グラフ（横軸週）

--- src/others/show_textgrid_labelratio.py
+++ src/others/show_textgrid_labelratio.py
@@ -100,120 +100,127 @@ if __name__ == "__main__":
 
     path = pathlib.Path("/datanet/users/muesaka/marmoset/Recorder")  # Marmosetの音声ディレクトリ（/あやぴょん, /あさぴょん, ...）
 
-    tag = ['UE', 'VPA']
+    # tag = ['UE', 'VPA']
+    tag = [3,4,5,6,7,8,9,10,11,12,13,14]
     # lab = ["Phee","Twitter","Others","Trill","Ek","Pr","Tsik"]
     lab = ["Phee","Trill","Twitter","Ek","Pr","Tsik","Others"]
-    data = []
-    index = []
-    for j,names in enumerate([tests+valids+trains, vpas]):
-    # for j,names in enumerate([temp]):
+
+    # UE，VPAのタグそれぞれで処理
+    # for j,names in enumerate([tests+valids+trains, vpas]):
+    for j,names in enumerate([temp]):
 
         print(tag[j])
-        # callの数え上げ用辞書
-        dict_label = call_init.copy()
 
+        # 個体名でforループ
         for name in names:
+
+            data = []
+            
             pattern = str(path) + "/" + name + "/*.TextGrid"
             weeks = glob.glob(pattern)
 
             # パターンマッチング 週抽出と並べ替え
-            # print("name=", name)
-            # print("pre=", len(weeks))
             for i,week in enumerate(weeks):
                 pattern = 'VOC_[0-9]+[-_][0-9]+_+[^_]*_+([^_]*).*'
                 weeks[i]= [week, re.findall(pattern ,week)[0].replace("W","")]
-
             weeks = sorted(weeks, key=lambda x: int(x[1]))
-            # pprint.pprint(weeks, width=100)
-            # print("post=", len(weeks))
-
             week_tmp = []
             for num in range(len(weeks)):
                 week_tmp.append(weeks[num][0])
             weeks = week_tmp
-            # pprint.pprint(weeks)
             
-      
-            
+
+            # 週ごとにforループ
+            lenweek = tag
+            for i in lenweek:
+
+                # callの数え上げ用辞書
+                dict_label = call_init.copy()
+
+                for week in weeks:
+                    pattern = 'VOC_[0-9]+[-_][0-9]+_+[^_]*_+([^_]*).*'
+                    j = re.findall(pattern ,week)[0].replace("W","")
+
+                    if str(i) != j:
+                        continue
+                    print(i,j)
+                    print(week)
+                    text = textgrid.TextGrid.fromFile(week) # text = [0:鳴き声, 1:ドア音][interval]    
+                    # TextGridのIntervalごと
+                    for k in range(len(text[0])):
+                        call = text[0][k].mark # Phee, Trill, ...
+                        call = call.replace(' ', '') # " Unknown" -> "Unknown"
+                        call = call.upper() # phee-trill -> PHEE-TRILL
+                        call = call.title() # PHEE-TRILL -> Phee-Trill
+
+                        # 空のIntervalはcontinue
+                        if call == "":
+                            continue
+
+                        # 辞書で修正
+                        call = correct_label[call]
+
+                        # いらないものならcontinue
+                        if (call == "Breath" or
+                            call == "Chirp" or
+                            call == "Chatter"):
+                            continue
+
+                        elif (call == "Phee-Trill" or
+                            call == "Trill-Phee"):
+                            call = "Pr"
+
+                        elif (call == "Cry" or
+                            call == "Cough" or
+                            call == "Unknown"):
+                            call = "Others"
+
+                        dict_label[call] = dict_label.get(call, 0) + 1 # {"Phee":0, "Trill":0, ...}
+                # 個体ごとに一度集計
+                data_split = []
+                total = sum(dict_label.values())
+                d={} #空辞書の定義
+                count = 0
+                for n in dict_label:
+                    if dict_label[n] == 0:
+                        d[n] = 0
+                    else:
+                        d[n] = dict_label[n] / total #割合の計算
+                        count += dict_label[n]
+                # d_ratio = sorted(d.items(), key=lambda x: x[0], reverse=True)
+                d_ratio = list(d.items())
+                for m in d_ratio:
+                    print(m[0].ljust(10), '{}'.format(m[1]))
+                    data_split.append(m[1])
+                print("count=",count)
+                print("")
+                print(len(data_split))
+                data.append(data_split)
         
-    
-            # print(name)
+        # 集計
+        # total = sum(dict_label.values())
+        # d={} #空辞書の定義
+        # count = 0
+        # for n in dict_label:
+        #     d[n] = dict_label[n] / total #割合の計算
+        #     count += dict_label[n]
+        # # d_ratio = sorted(d.items(), key=lambda x: x[1], reverse=True)
+        # d_ratio = list(d.items())
+        # for m in d_ratio:
+        #     print(m[0].ljust(10), '{}'.format(m[1]))
+        #     data.append(m[1])
+        # print("count=",count)
+        # print("")
 
-            # callの数え上げ用辞書
-            # dict_label = call_init.copy()
-            # 週ごと
-            for week in weeks:
-                # print(week)
-                text = textgrid.TextGrid.fromFile(week) # text = [0:鳴き声, 1:ドア音][interval]    
-                # TextGridのIntervalごと
-                for k in range(len(text[0])):
-                    call = text[0][k].mark # Phee, Trill, ...
-                    call = call.replace(' ', '') # " Unknown" -> "Unknown"
-                    call = call.upper() # phee-trill -> PHEE-TRILL
-                    call = call.title() # PHEE-TRILL -> Phee-Trill
 
-                    # 空のIntervalはcontinue
-                    if call == "":
-                        continue
-
-                    # 辞書で修正
-                    call = correct_label[call]
-
-                    # いらないものならcontinue
-                    if (call == "Breath" or
-                        call == "Chirp" or
-                        call == "Chatter"):
-                        continue
-
-                    elif (call == "Phee-Trill" or
-                          call == "Trill-Phee"):
-                          call = "Pr"
-
-                    elif (call == "Cry" or
-                          call == "Cough" or
-                          call == "Unknown"):
-                          call = "Others"
-
-                    dict_label[call] = dict_label.get(call, 0) + 1 # {"Phee":0, "Trill":0, ...}
-            # 個体ごとに一度集計
-            # total = sum(dict_label.values())
-            # d={} #空辞書の定義
-            # for n in dict_label:
-            #     if dict_label[n] == 0:
-            #         d[n] = 0
-            #     else:
-            #         d[n] = dict_label[n] / total #割合の計算
-            # # d_ratio = sorted(d.items(), key=lambda x: x[0], reverse=True)
-            # d_ratio = list(d.items())
-            # for m in d_ratio:
-            #     print(m[0].ljust(10), '{}'.format(m[1]))
-            # print("")
-                
-        total = sum(dict_label.values())
-        d={} #空辞書の定義
-        count = 0
-        for n in dict_label:
-            d[n] = dict_label[n] / total #割合の計算
-            count += dict_label[n]
-        # d_ratio = sorted(d.items(), key=lambda x: x[1], reverse=True)
-        d_ratio = list(d.items())
-        for m in d_ratio:
-            print(m[0].ljust(10), '{}'.format(m[1]))
-            data.append(m[1])
-        print("count=",count)
-        print("")
-
-    # 積み上げ棒グラフ
-    dataset = pd.DataFrame([data[0:7], data[7:14]], 
-                       index=tag, 
-                       columns=lab)
-    print(dataset)
-    bottom = np.zeros_like(dataset.index)
-    for name in dataset.columns:
-        plt.bar(dataset.index, dataset[name], bottom=bottom, label=name)
-        bottom += dataset[name]
-    
-    plt.legend() 
-    plt.savefig("./LabelRatio/labelratio.pdf")
-    plt.close()
+            # 積み上げ棒グラフ
+            dataset = pd.DataFrame(data, 
+                            index=tag, 
+                            columns=lab)
+            print(dataset)
+            print("")
+            dataset.plot.bar(stacked=True)
+            plt.legend() 
+            plt.savefig("./LabelRatio/LabelRatioWeeks_{}.pdf".format(name))
+            plt.close()
     
